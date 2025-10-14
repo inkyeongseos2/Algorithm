7049e1d05cdb857ed8c204912c85dbe9ed1f1f2a
inkyeongseos2 <150255041+inkyeongseos2@users.noreply.github.com>
2025-10-14 20:22:28 +0900

    [level 2] Title: 괄호 회전하기, Time: 38.14 ms, Memory: 92.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        for(int i = 0; i< s.length(); i++){
+            String chword = s.substring(i) + s.substring(0,i);
+            if(invaild(chword)) answer++;
+        }
+        return answer;
+    }
+    
+    public boolean invaild(String str){
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : str.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+                
+                char top = stack.pop();
+                if((c == ')' && top !='(') ||
+                   (c == ']' && top !='[') ||
+                   (c == '}' && top !='{')) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
